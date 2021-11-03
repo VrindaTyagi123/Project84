033133028b779f6a401281f1952da974bc572447
VrindaTyagi123 <83289238+VrindaTyagi123@users.noreply.github.com>
2021-11-03 17:21:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
 
+// Create a reference for the canvas
+
 img_width = 300;
 img_height = 100;
 
@@ -10,9 +12,9 @@ img_x = 100;
 img_y = 100;
 
 function add() {
-	img_imgTag = new Image(); 
-	img_imgTag.onload = uploadimg; 
-	img_imgTag.src = img_image; 
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
 }
 
 function uploadimg() {
@@ -20,7 +22,7 @@ function uploadimg() {
 	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
 
-window.addEventListener("keydown", my_keydown);
+//Write a code to grab the key-pressed event
 
 function my_keydown(e)
 {
@@ -28,58 +30,35 @@ function my_keydown(e)
 	console.log(keyPressed);
 	
 		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		//write a code to check the type of key pressed
 
-		aplhabetkey();
-		document.getElementById("d1").innerHTML="You pressed Alphabet Key";
-		console.log("alphabet key");
-
-		numberkey();
-		document.getElementById("d1").innerHTML="You pressed Number Key";
-		console.log("number key");
-
-		arrowkey();
-		document.getElementById("d1").innerHTML="You pressed Arrow Key";
-		console.log("arrow key");
-
-		specialkey();
-		document.getElementById("d1").innerHTML="You pressed Special Key";
-		console.log("special key");
 	{
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
-		console.log("other key");
 	}
 }
 
 function aplhabetkey()
 {
-	img_image = "Alpkey.png";
+	img_image="Alpkey.png";
 	add();
 
 }
-
-
 function numberkey()
 {
-	img_image = "numkey.png";
+	img_image="numkey.png";
 	add();
 }
-
-
 function arrowkey()
 {
-	img_image = "Arrkey.png";
+	img_image="Arrkey.png";
 	add();
 }
-
-
 function specialkey()
 {
-	img_image = "spkey.png";	
+	img_image="spkey.png";
 	add();
 }
-
-
 function otherkey()
 {
 	img_image="otherkey.png";
